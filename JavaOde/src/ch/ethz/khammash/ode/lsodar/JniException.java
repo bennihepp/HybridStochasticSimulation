4e2f5ab25c9560f45514772204441e8263b4cccc
Benjamin Hepp <benjamin.hepp@gmail.com>
2013-07-24 17:46:34 +0200

    Added binaries to git repo

---
+++ JavaOde/src/ch/ethz/khammash/ode/lsodar/JniException.java
@@ -0,0 +1,14 @@
+package ch.ethz.khammash.ode.lsodar;
+
+import ch.ethz.khammash.ode.Exception;
+
+
+public class JniException extends Exception {
+
+	private static final long serialVersionUID = -6143760954096210505L;
+
+	public JniException(String message) {
+		super(message);
+	}
+
+}
